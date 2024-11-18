e56630cd8b8bcd79f9ed3596871d0a359a6c7989
SNFT <info@snft.pro>
2024-11-18 23:59:10 +0200

    Update user-status.tsx

--- components/user-status.tsx
+++ components/user-status.tsx
@@ -120,7 +120,7 @@ export default function UserStatus() {
                                 setNewStatus(e.target.value)
                                 setCharacterCount(e.target.value.length)
                             }}
-                            placeholder="What is happening!"
+                            placeholder="Onc-hain message, cannot be deleted!"
                         />
                         <div className={styles.characterCountContainer}>
                             <p className={characterDecoration}>{characterCount}/140</p>
